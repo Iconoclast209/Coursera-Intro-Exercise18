0021b3ae3a31a7b5bf4eb488285b04aafa424e8a
asmith <mrpresidentsmith@gmail.com>
2019-04-14 10:39:02 -0700

    Assignment completed.

---
+++ Exercise18/Assets/Scripts/Jumper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    [SerializeField]
+    GameObject character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveCharacter();
+        }
+    }
+
+    void MoveCharacter()
+    {
+        //Get Mouse Position
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = -Camera.main.transform.position.z;
+
+        //Convert Mouse Position to desired character position
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        //Move character to that position
+        character.transform.position = newPosition;
+    }
+}
